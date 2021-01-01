f74c44a2dc0ceab1a69c8316fd89d26441c4023c
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2021-01-01 14:41:46 +0000

    custom toasts implemented

--- app/src/main/java/com/frezzcoding/kotlincertprep/MainActivity.kt
+++ app/src/main/java/com/frezzcoding/kotlincertprep/MainActivity.kt
@@ -2,10 +2,48 @@ package com.frezzcoding.kotlincertprep
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.custom_toast.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //what to cover
+        //todo toasts
+        showToast()
+        //todo snackbars
+        //todo localisation
+        //todo app fundamentals
+        //todo create notification
+        //todo androidx overview
+        //todo getting started with jetpack
+        //todo android ktx
+        //todo codelabs workmanager
+        //todo codelabs notifications
+
     }
+
+
+    private fun showToast(){
+        val layout: View = layoutInflater.inflate(R.layout.custom_toast, custom_toast_container)
+        val text: TextView = layout.findViewById(R.id.text)
+        text.text = "This is a toaster!"
+
+        with (Toast(applicationContext)) {
+            setGravity(Gravity.CENTER_VERTICAL, 0, 0)
+            duration = Toast.LENGTH_LONG
+            view = layout
+            show()
+        }
+
+        val toast = Toast.makeText(applicationContext, "This is a toaster!", Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.TOP, 0,0)
+    }
+
 }
